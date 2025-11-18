2b12de507be9481bbde031705d5dda4260d052de
mohamedapdELsalam <moalgouker@gmail.com>
2025-11-19 00:22:59 +0200

    update

--- backend_nodejs/model/course.model.js
+++ backend_nodejs/model/course.model.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
-<<<<<<< HEAD
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     image: { type: String, required: true },
     instructor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     language: { type: String },
-    level: { type: String, enum: [1,2,3] },
+    level: { type: Number, enum: [1,2,3] },
     duration: { type: String, default: 0 },
     lessons: [{ type: mongoose.Schema.Types.ObjectId, ref: "Lesson" }],
     enrolledCount: { type: Number, default: 0 },
@@ -17,37 +16,3 @@ const courseSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model("Course", courseSchema);
-=======
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true
-    }, image: {
-        type: String,
-        required: true,
-    }, instructor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required:true
-    }, language: {
-        type: String,
-    }, level: {
-        type: String,
-        enum: ["متقدم", "متوسط", "مبتدأ"]
-    },duration:{
-        type :String,
-        default:0
-    },lessons:[{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Lesson"
-    }]
-});
-module.exports = mongoose.model("Course", courseSchema);
->>>>>>> c04325d (Add frontend files and restructure project)
